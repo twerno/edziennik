4453b2134c94cf997c393adfc8db2c3944e94931
Tomasz Werno <twerno@gmail.com>
2009-01-07 20:20:47 +0100

    ...

---
+++ db/migrate/20090107084356_domyslne_konta.rb
@@ -0,0 +1,40 @@
+class DomyslneKonta < ActiveRecord::Migration
+  def self.up
+    u = Uczen.new :imie => "uczen", :nazwisko => "uczen", :pesel => "uczen"
+    r = Rodzic.new :imie_ojca => "rodzic", :imie_matki => "rodzic", :pesel => "rodzic"
+    r.save
+    u.rodzic_id = r.id
+    u.save
+    
+    n = Nauczyciel.new :imie => "nauczyciel", :nazwisko => "nauczyciel", :pesel => "nauczyciel"
+    n.save
+    
+    us = User.new
+    us.login = "uczen"
+    us.password = "uczen"
+    us.uczen_id = u.id
+    us.save(false)
+    us.register!
+    us.activate!
+    
+    us = User.new
+    us.login = "rodzic"
+    us.password = "rodzic"
+    us.rodzic_id = r.id
+    us.save(false)
+    us.register!
+    us.activate!
+    
+    us = User.new
+    us.login = "nauczyciel"
+    us.password = "nauczyciel"
+    us.rodzic_id = n.id
+    us.save(false)
+    us.register!
+    us.activate!
+
+  end
+
+  def self.down
+  end
+end
