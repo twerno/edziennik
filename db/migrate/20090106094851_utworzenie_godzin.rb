7139e855aac810db1f8d4f7bd3e5a5798d4918d8
Tomasz Werno <twerno@gmail.com>
2009-01-10 17:14:38 +0100

    poprawki

--- db/migrate/20090106094851_utworzenie_godzin.rb
+++ db/migrate/20090106094851_utworzenie_godzin.rb
@@ -1,56 +1,57 @@
 class UtworzenieGodzin < ActiveRecord::Migration
   def self.up
+    u = User.new :id => 1
     g = Godzina.new :begin => "8:00", :end => "8:45", :nazwa => "1"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "9:00", :end => "9:45", :nazwa => "2"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "10:00", :end => "10:45", :nazwa => "3"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "11:00", :end => "11:45", :nazwa => "4"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "12:00", :end => "12:45", :nazwa => "5"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "13:00", :end => "13:45", :nazwa => "6"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "14:00", :end => "14:45", :nazwa => "7"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "15:00", :end => "15:45", :nazwa => "8"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "16:00", :end => "16:45", :nazwa => "9"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "17:00", :end => "17:45", :nazwa => "10"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "18:00", :end => "18:45", :nazwa => "11"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "19:00", :end => "19:45", :nazwa => "12"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
     g = Godzina.new :begin => "20:00", :end => "20:45", :nazwa => "13"
     g.set_editors_stamp "db:migrate"
-    g.set_current_user User.first
+    g.set_current_user u
     g.save
   end
 
